bf1f58e8853db711de20f5fa3cdb11c546352b02
Potato Hatsue <1793913507@qq.com>
2021-01-19 19:22:32 +0800

    workaround fcitx5

--- modules/system.nix
+++ modules/system.nix
@@ -10,7 +10,13 @@
 
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-chinese-addons fcitx5-material-color ];
+
+    fcitx5.addons = with pkgs; [
+      fcitx5-chinese-addons
+      fcitx5-material-color
+      fcitx5-pinyin-zhwiki
+      fcitx5-pinyin-moegirl
+    ];
   };
 
   environment.systemPackages = with pkgs; [
