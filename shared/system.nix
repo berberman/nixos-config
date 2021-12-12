47e5c34400c431288d425532bebcd7013545bc48
Potato Hatsue <1793913507@qq.com>
2021-12-12 17:06:07 -0500

    Enable pipewire with bluetooth

--- shared/system.nix
+++ shared/system.nix
@@ -2,9 +2,13 @@
 
 {
 
-  sound.enable = true;
-
-  hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   services.openssh.enable = true;
 
