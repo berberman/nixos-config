1d1c8f5da7548e6ab4645bec4cf33d8a44b207d0
Potato Hatsue <1793913507@qq.com>
2022-01-25 00:01:00 -0500

    Add wgcf

--- nr/networking.nix
+++ nr/networking.nix
@@ -6,5 +6,19 @@
     useDHCP = false;
     networkmanager.enable = true;
     firewall.enable = false;
+    wg-quick.interfaces = {
+      wgcf = {
+        address =
+          [ "172.16.0.2/32" "fd01:5ca1:ab1e:8962:bfd4:20b5:bf0c:f259/128" ];
+        dns = [ "1.1.1.1" ];
+        privateKeyFile = "/home/berberman/.wgcf/a.key";
+        mtu = 1280;
+        peers = [{
+          publicKey = "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=";
+          allowedIPs = [ "0.0.0.0/0" "::/0" ];
+          endpoint = "162.159.192.1:2408";
+        }];
+      };
+    };
   };
 }
--- shared/system.nix
+++ shared/system.nix
@@ -51,6 +51,7 @@
     gparted
     ripgrep
     kdeconnect
+    pciutils
   ];
 
 }
