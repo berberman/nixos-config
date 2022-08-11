359c17d1ca778395a9ba9b1305f323a5da02b73a
Potato Hatsue <1793913507@qq.com>
2022-08-11 19:19:14 -0400

    Add owner and group for matrix secret

--- secrets/default.nix
+++ secrets/default.nix
@@ -3,5 +3,11 @@
     listToAttrs (map (n: {
       name = removeSuffix ".age" n;
       value = { file = ./. + ("/" + n); };
-    }) (attrNames (import ./secrets.nix)));
+    }) (attrNames (import ./secrets.nix))) // {
+      matrix-synapse-registration = {
+        file = ./matrix-synapse-registration.age;
+        owner = "matrix-synapse";
+        group = "matrix-synapse";
+      };
+    };
 }
