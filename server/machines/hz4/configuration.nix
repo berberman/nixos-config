791717148148146216f7562491fcef90591753b1
Potato Hatsue <1793913507@qq.com>
2022-08-11 18:48:41 -0400

    Add acme email

--- server/machines/hz4/configuration.nix
+++ server/machines/hz4/configuration.nix
@@ -2,4 +2,5 @@
   imports = [ ./hardware-configuration.nix ./matrix.nix ];
   networking.hostName = "POTATO-HZ4";
   security.acme.acceptTerms = true;
+  security.acme.defaults.email = "acme@typed.icu";
 }
