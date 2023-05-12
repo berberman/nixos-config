d79770e79a3c54feee6cbf129107dd3fa21c9666
Potato Hatsue <1793913507@qq.com>
2023-05-12 20:09:54 +0800

    Use bfsu

--- desktop/machines/nn/nix.nix
+++ desktop/machines/nn/nix.nix
@@ -1,7 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  nix.settings.substituters =
-    lib.mkBefore [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
+  nix.settings = {
+    substituters =
+      lib.mkBefore [ "https://mirrors.bfsu.edu.cn/nix-channels/store" ];
+  };
 }
 
