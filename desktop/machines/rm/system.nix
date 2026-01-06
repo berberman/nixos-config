c1e120eb1dbf3a4eaee6ede1bcb0b87aa2a44e10
Potato Hatsue <1793913507@qq.com>
2026-01-06 13:08:14 -0800

    Remove openconnect override

--- desktop/machines/rm/system.nix
+++ desktop/machines/rm/system.nix
@@ -8,17 +8,17 @@
     enable = true;
     dockerCompat = true;
   };
-  system.replaceDependencies.replacements = [{
-    oldDependency = pkgs.openconnect;
-    newDependency = pkgs.openconnect.overrideAttrs (old: {
-      src = pkgs.fetchFromGitLab {
-        owner = "openconnect";
-        repo = "openconnect";
-        rev = "f17fe20d337b400b476a73326de642a9f63b59c8";
-        hash = "sha256-OBEojqOf7cmGtDa9ToPaJUHrmBhq19/CyZ5agbP7WUw=";
-      };
-    });
-  }];
+  # system.replaceDependencies.replacements = [{
+  #   oldDependency = pkgs.openconnect;
+  #   newDependency = pkgs.openconnect.overrideAttrs (old: {
+  #     src = pkgs.fetchFromGitLab {
+  #       owner = "openconnect";
+  #       repo = "openconnect";
+  #       rev = "f17fe20d337b400b476a73326de642a9f63b59c8";
+  #       hash = "sha256-OBEojqOf7cmGtDa9ToPaJUHrmBhq19/CyZ5agbP7WUw=";
+  #     };
+  #   });
+  # }];
   niri = {
     enable = true;
     ex = ''
